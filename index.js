1006aa8b5c19021464c42d6c39f31347aa674845
Muhammad Ubaid <62153753+Ubaid-arif@users.noreply.github.com>
2022-09-10 12:11:08 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("port is runing 3000");
+});
+
+app.use("/ads", (req,res) => {
+  const ads = [
+    { title: "iphone", price: 5000 },
+    { title: "Car", price: 9000 },
+    { title: "laptop", price: 8000 },
+  ];
+  res.send(ads);
+
+});
+//Data Base
+
