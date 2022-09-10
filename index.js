6b9bac6ac81687a53ccf26773e62abc967a59685
Muhammad Ubaid <giftprintingservice@gmail.com>
2022-09-10 12:26:18 +0500

    Done
    done

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ app.listen(3000, () => {
   console.log("port is runing 3000");
 });
 
-app.use("/ads", (req,res) => {
+app.use("/ads", (req, res) => {
   const ads = [
+    { title: "iphone", price: 5000 },
+    { title: "Car", price: 9000 },
+    { title: "laptop", price: 8000 },
+    { title: "iphone", price: 5000 },
+    { title: "Car", price: 9000 },
+    { title: "laptop", price: 8000 },
     { title: "iphone", price: 5000 },
     { title: "Car", price: 9000 },
     { title: "laptop", price: 8000 },
   ];
   res.send(ads);
-
 });
 //Data Base
-
